3f0dcf3e74e4875adbd852749b2c78d4c88044c1
vinusha <vinushareddy.maddhuri14@gmail.com>
2021-10-03 01:44:50 -0400

    Added Thumbnail query parameter test cases

--- src/test/java/queryParameterTesting/StartAndEndDateQueryParameterTesting.java
+++ src/test/java/queryParameterTesting/StartAndEndDateQueryParameterTesting.java
@@ -1,5 +1,101 @@
 package queryParameterTesting;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
 public class StartAndEndDateQueryParameterTesting {
+    public static RestTemplate restTemplate;
+    public static String baseUrl = "https://api.nasa.gov";
+    public static String apiKey = "cvxjev456MzhfJbw9JFrZOJIvMRByegGqqcbmFMI";
+    public static String templateUrl = baseUrl + "/planetary/apod?api_key=";
+    public static String response;
+
+    @BeforeClass
+    public static void setUpRestTemplate() {
+        restTemplate = new RestTemplate();
+    }
+    /*
+    Test API response given a valid start date
+     */
+    @Test
+    public void testResponseGivenAStartDate() throws ParseException {
+        String dateValue = LocalDate.now().minusDays(1).toString();
+        String startDateParamExtension = "&start_date=" + dateValue;
+        response = restTemplate.getForObject(templateUrl + apiKey + startDateParamExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONArray object = (JSONArray) parser.parse(response);
+        Assert.assertEquals("Response should return " + 2 + "images information", 2, object.size());
+    }
+
+    /*
+    Test the API response with valid start and end date range
+     */
+    @Test
+    public void testResponseGivenARangeOfDates() throws ParseException {
+        int rangeOfDays = 9;
+        LocalDate startDate = LocalDate.now().minusDays(10);
+        String endDateValue = startDate.plusDays(rangeOfDays).toString();
+        String dateParamExtension = "&start_date=" + startDate + "&end_date=" + endDateValue;
+        response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONArray object = (JSONArray) parser.parse(response);
+        Assert.assertEquals("Response should return " + rangeOfDays + 1 + "images information", rangeOfDays + 1, object.size());
+    }
+
+    /*
+  Test the API response with invalid start and end date range
+   */
+    @Test
+    public void testResponseGivenARangeOfInvalidDates(){
+        String startDate = "1990-02-20";
+        String endDate = "2030-01-09";
+        String dateParamExtension = "&start_date=" + startDate + "&end_date=" + endDate;
+        try {
+            response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension, String.class);
+        } catch (HttpClientErrorException ex) {
+            Assert.assertEquals("Bad request response code should be returned from server", 400, ex.getRawStatusCode());
+            System.out.println(ex.getMessage());
+        }
+    }
+    /*
+     Test the API response with end date later than the start date
+    */
+    @Test
+    public void testResponseStartDateGreaterThanEndDate(){
+        String startDate = "2021-01-09";
+        String endDate = "1997-02-20";
+        String dateParamExtension = "&start_date=" + startDate + "&end_date=" + endDate;
+        try {
+            response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension, String.class);
+        } catch (HttpClientErrorException ex) {
+            Assert.assertEquals("Bad request response code should be returned from server", 400, ex.getRawStatusCode());
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /*
+    Test the API response with a combination of date, start date and end date
+    */
+    @Test
+    public void testResponseWithDateStartDateAndEndDate(){
+        String startDate = "1997-02-20";
+        String endDate = "2021-01-09";
+        String date = LocalDate.now().toString();
+        String dateParamExtension = "&start_date=" + startDate + "&end_date=" + endDate + "&date=" + date;
+        try {
+            response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension, String.class);
+        } catch (HttpClientErrorException ex) {
+            Assert.assertEquals("Bad request response code should be returned from server", 400, ex.getRawStatusCode());
+            System.out.println(ex.getMessage());
+        }
+    }
 
 }
---
+++ src/test/java/queryParameterTesting/ThumbnailQueryParameterTesting.java
@@ -0,0 +1,69 @@
+package queryParameterTesting;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+public class ThumbnailQueryParameterTesting {
+    public static RestTemplate restTemplate;
+    public static String baseUrl = "https://api.nasa.gov";
+    public static String apiKey = "cvxjev456MzhfJbw9JFrZOJIvMRByegGqqcbmFMI";
+    public static String templateUrl = baseUrl + "/planetary/apod?api_key=";
+    public static String response;
+
+    @BeforeClass
+    public static void setUpRestTemplate() {
+        restTemplate = new RestTemplate();
+    }
+    @Test
+    public void testThumbnailQueryParameterTrue() throws ParseException {
+        String dateValue = "2018-02-26";
+        String dateParamExtension = "&date=" + dateValue;
+        String thumbnailExtension = "&thumbs="+true;
+        response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension + thumbnailExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(response);
+        String thumbnailUrlValue = (String)object.get("thumbnail_url");
+        Assert.assertNotNull("Thumbnail url should not be null",thumbnailUrlValue);
+    }
+    @Test
+    public void testThumbnailQueryParameterTrueForNonVideoFiles() throws ParseException {
+        String dateValue = "2018-01-26";
+        String dateParamExtension = "&date=" + dateValue;
+        String thumbnailExtension = "&thumbs="+true;
+        response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension + thumbnailExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(response);
+        String thumbnailUrlValue = (String)object.get("thumbnail_url");
+        Assert.assertNull("Thumbnail url should not be null",thumbnailUrlValue);
+    }
+    @Test
+    public void testThumbnailQueryParameterFalseForVideoFiles() throws ParseException {
+        String dateValue = "2018-02-26";
+        String dateParamExtension = "&date=" + dateValue;
+        String thumbnailExtension = "&thumbs="+false;
+        response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension + thumbnailExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(response);
+        String thumbnailUrlValue = (String)object.get("thumbnail_url");
+        Assert.assertNull("Thumbnail url should not be null",thumbnailUrlValue);
+    }
+    @Test
+    public void testThumbnailQueryParameterFalseForNonVideoFiles() throws ParseException {
+        String dateValue = "2018-12-26";
+        String dateParamExtension = "&date=" + dateValue;
+        String thumbnailExtension = "&thumbs="+false;
+        response = restTemplate.getForObject(templateUrl + apiKey + dateParamExtension + thumbnailExtension, String.class);
+        JSONParser parser = new JSONParser();
+        JSONObject object = (JSONObject) parser.parse(response);
+        String thumbnailUrlValue = (String)object.get("thumbnail_url");
+        Assert.assertNull("Thumbnail url should not be null",thumbnailUrlValue);
+    }
+}
